07ed24e0a604c9f7f5544d939ffc104b00497ab9
Anatoly Balbutsky <bal.anatolii1985@mail.ru>
2023-04-04 10:13:36 +0300

    l2 z1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 16000;
+        int miles;
+        int bonus = 20;
+        miles = priceTicket / bonus;
+        System.out.println(miles);
+    }
+}
